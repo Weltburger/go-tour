a20b96322d61a821b6e0bf80150be153ad2c3138
Weltburger <pakowski398@yandex.ru>
2021-09-01 09:40:28 +0300

    loops and functions ex.1.2

--- loops_and_functions/main.go
+++ loops_and_functions/main.go
@@ -5,12 +5,22 @@ import (
 )
 
 func Sqrt(x float64) float64 {
-	z := 1.0
-	for i := 0; i < 10; i++ {
-		z -= (z*z - x) / (2*z)
+	z, prev := 1.0, 0.0
+	for {
+		prev, z = z, z - (z*z - x) / (2*z)
 		fmt.Println(z)
+		if abs(prev - z) < 1e-15 {
+			break
+		}
 	}
 	return z
+}
+
+func abs(n float64) float64 {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
 
 func main() {
