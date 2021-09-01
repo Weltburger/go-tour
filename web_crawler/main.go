78316a2728db7defd3487c7c770dc8b81d25763c
Weltburger <pakowski398@yandex.ru>
2021-09-01 19:42:42 +0300

    web crawler

---
+++ web_crawler/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Fetcher interface {
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+type Memory struct {
+	memory map[string]bool
+	mux     *sync.Mutex
+}
+
+func (m *Memory) isFetched(url string) bool {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	_, ok := m.memory[url]
+	if ok {
+		return true
+	}
+
+	m.memory[url] = true
+	return false
+}
+
+var memory = &Memory{
+	memory: make(map[string]bool),
+	mux:    new(sync.Mutex),
+}
+
+func Crawl(url string, depth int, fetcher Fetcher, wg *sync.WaitGroup) {
+	defer wg.Done()
+	if depth <= 0 || memory.isFetched(url) {
+		return
+	}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("found: %s %q\n", url, body)
+	for _, u := range urls {
+		wg.Add(1)
+		go Crawl(u, depth-1, fetcher, wg)
+	}
+	return
+}
+
+func main() {
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+
+	go Crawl("https://golang.org/", 4, fetcher, wg)
+
+	wg.Wait()
+}
+
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
